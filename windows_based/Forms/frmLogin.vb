01dd395962488b40f9c26d8ee0c57b1f16db53a7
rizky18 <r1218ok@gmail.com>
2013-12-18 17:14:29 +0700

    Update frmLogin.vb

--- windows_based/Forms/frmLogin.vb
+++ windows_based/Forms/frmLogin.vb
@@ -1,4 +1,5 @@
-﻿Public Class frmLogin
+Imports MySql.Data.MySqlClient
+Public Class frmLogin
     'fungsi untuk drag form
     Dim IsDraggingForm As Boolean = False
     Private MousePos As New System.Drawing.Point(0, 0)
@@ -21,19 +22,48 @@
 
     Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
         Me.Close()
-        frmServerSettings.Close()
+        frmLocalServerSettings.Close()
         frmMain.Close()
     End Sub
 
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
-        Me.Close()
-        frmServerSettings.Close()
-        frmMain.Show()
-        frmMain.timerShow.Start()
+        Dim cek As Integer = 0
+        Try
+            functions.localConnect()
+            Dim cmd As MySqlDataReader
+            cmd = New MySqlCommand("Select password from user where username ='" + txtUsername.Text + "'", functions.localConnection).ExecuteReader()
+            Try
+                cmd.Read()
+                If (cmd.GetString(0) = txtPassword.Text) Then
+                    cek = 1
+                End If
+                cmd.Close()
+            Catch ex As Exception
+                cek = 0
+                cmd.Close()
+            End Try
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+            cek = 2
+        End Try
+
+        If (cek = 1) Then
+            Me.Close()
+            frmLocalServerSettings.Close()
+            frmMain.Show()
+            frmMain.timerShow.Start()
+            variabels.oper = txtUsername.Text
+            variabels.periode = functions.getPeriode
+            variabels.periodeAktif = functions.getPeriode
+        ElseIf (cek = 0) Then
+            MessageBox.Show("Username atau Password salah!")
+        ElseIf (cek = 2) Then
+            MessageBox.Show("Koneksi Bermasalah!")
+        End If
     End Sub
 
     Private Sub btnServerSettings_Click(sender As Object, e As EventArgs) Handles btnServerSettings.Click
-        frmServerSettings.ShowDialog()
+        frmLocalServerSettings.ShowDialog()
     End Sub
 
     Private Sub timerShow_Tick(sender As Object, e As EventArgs) Handles timerShow.Tick
