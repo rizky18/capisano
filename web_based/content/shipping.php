00fd75ae5dbf77ae3d72ed12c7a7968b9fe3facb
rizky18 <r1218ok@gmail.com>
2013-12-18 09:49:44 +0700

    Create shipping.php

---
+++ web_based/content/shipping.php
@@ -0,0 +1,34 @@
+<!--==============================content================================-->
+    <section id="content"><div class="ic"></div>
+        <div class="main">
+            <div class="wrapper">
+            	<article class="col-1">
+                	<div class="indent-left">
+                        <div class="p2">
+                            <h3 class="p1">Ongkos Kirim</h3>
+                            <dl class="list-2">
+                                <dt><em>Area</em><span>Harga</span></dt>
+                                <dd><em>A</em><span>Rp &nbsp; 5.000,-</span></dd>
+                                <dd><em>B</em><span>Rp 10.000,-</span></dd>
+                                <dd><em>C</em><span>Rp 15.000,-</span></dd>
+                                <dd><em>D</em><span>Rp 20.000,-</span></dd>                    
+                            </dl>
+                        </div>
+                        
+                    </div>
+                </article>
+                <article class="col-2">
+                	<h3 class="p1">Pengiriman</h3>
+                    <p>kami memberikan pelayanan pengiriman kepada konsumen</p>
+                    <figure class="img-indent-bot">
+                        <img src="images/shipping.jpg" width="600" height="400"/>
+                    </figure>
+                    
+                    <h6 class="p2">pengiriman hanya dilakukan selama jam kerja, 
+					area pengiriman di bagi 4 bagian yaitu : A,B,C dan D
+					setiap area dikenakan tarif yang berbeda. </h6>
+                    untuk luar daerah, pengiriman dilakukan melalui tiki/JNE
+                </article>
+            </div>
+        </div>
+    </section>
